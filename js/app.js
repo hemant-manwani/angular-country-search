23f8c0c8ae7a95b335c064fcf5dc33a4ee1851b6
Hemant <hemantmanwani.iitr@gmail.com>
2017-01-09 14:33:51 +0530

    Search country using angular-search plugin

---
+++ js/app.js
@@ -0,0 +1,7 @@
+﻿var appRoot = angular.module('search-country-demo', ['angular-search-box']);     //Define the main module
+
+appRoot.run(function ($rootScope) {
+    angular.element(document).on("click", function (e) {
+        $rootScope.$broadcast("documentClicked", angular.element(e.target));
+    });
+});
---
+++ js/controller.js
@@ -0,0 +1,31 @@
+﻿(function () {
+  'use strict';
+  appRoot.controller('countryController', ['$scope', 'countryService', function ($scope, countryService) {
+    var fetchingRecords = false;
+    $scope.getCountries = function (searchKey, pagenumber) {
+      if (fetchingRecords)
+        return;
+      fetchingRecords = true;
+      countryService.getCountries(searchKey, pagenumber)
+      .then(function (result) {
+          if (pagenumber === 1) {
+            $scope.totalRecords = result.TotalRecords;
+            $scope.countries = result.Records;
+          }else {
+            $scope.countries = $scope.countries.concat(result.Records);
+          }
+          fetchingRecords = false;
+      },function (errorMessage) {
+        window.console.warn(errorMessage);
+        fetchingRecords = false;
+      });
+    };
+    $scope.countries = [];
+    $scope.country = {
+        Key: "India",
+        Value: "In"
+    };
+    $scope.getCountries("", 1);
+
+  }]);
+}());
